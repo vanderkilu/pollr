c97c6ba3f23156a3d18c700928749442769f99a3
vanderkilu <vanderkiliku@gmail.com>
2019-01-18 17:26:11 +0000

    refactor the pollcreate component

--- src/components/PollCreate.vue
+++ src/components/PollCreate.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="text-center">
-        <div class="form-container">
-            <p :class="{errors:isError}" class="hide"> please you can't leave any of the field blank </p>
-            <p class="instructions">create polls by adding a question <br> and two alternatives </p>
-            <form @submit.prevent="createPoll" class="poll-create">
-                <input @focus="isError = false"  type="text" placeholder="question" v-model="poll">
-                <input v-for="alt in alts " :key="alt"
-                    type="text" 
-                    placeholder= "option"
-                    v-model="alt.value">
-                <button @click.prevent="addOption" class="btn btn-custom mb-5"> add Option </button>
-                <button type="submit" class="btn btn-custom"> Create Poll </button>
-            </form>
+    <div class="wrapper">
+        <form action="" class="form">
+            <p class="wrapper__text"> Let us poll together <span class="emoji"> 😇</span> </p>
+            <input type="text" class="input" placeholder="question">
+            <transition-group name="list">
+                    <input type="text" class="input" :placeholder="opt.value + i"  v-for="(opt,i) in alts" :key="i">
+            </transition-group>
+            <div class="option-controls">
+                <span class="add" @click="addOption">&plus;</span>
+                <span class="remove" @click="removeOption">&times;</span>
+            </div>
+            <button class="btn-auth btn-auth-colored">create poll</button>
+        </form>
+        <div class="wrapper-side">
+            Create your poll now! <span class="emoji"> 😇</span>
         </div>
     </div>
 </template>
@@ -23,7 +25,7 @@
             return {
                 poll: '',
                 isError: false,
-                alts: [{value: ''},{value: ''}]
+                alts: [{value: 'option '},{value: 'option '}]
             }
         },
         computed: {
@@ -56,25 +58,83 @@
                     .catch(err => console.log(err));
             },
             addOption() {
-                this.alts.push({value: ''});
-                console.log(this.validate());
+                this.alts.push({value: 'option '});
+            },
+            removeOption() {
+                this.alts = this.alts.slice(0, this.alts.length-1);
             }
         }
 
     }
 </script>
 <style scoped>
-.instructions {
-    font-size: 0.9rem;
-}
-.hide {
-    opacity: 0;
-}
-.errors {
-    opacity: 1;
-    background-color: #ffcdd2;
-    font-size: 0.9rem;
+.wrapper {
+    background-color: white;
     border-radius: 3px;
-    padding: 1rem;
+    box-shadow: 0 1rem 2rem rgba(0,0,0,0.04);
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    margin: 10rem 0;
 }
+.form {
+    padding: 8rem 3rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.wrapper-side {
+    padding: 8rem 3rem;
+    background-color: #673ab7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    color: white;
+    text-align: center;
+
+}
+.wrapper__text {
+    font-size: 1.6rem;
+    margin-bottom: 2rem;
+}
+.emoji {
+    color: #d81b60;
+    font-size: 1.4rem;
+}
+.add, .remove {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    cursor: pointer;
+    padding: 1rem;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    color: #673ab7;
+    background-color: #ede7f6;
+    box-shadow: 0 1rem 2rem rgba(0,0,0,0.04);
+    margin: 0 1rem;
+}
+.remove {
+    background-color: #fce4ec;
+    color: #d81b60;
+}
+.option-controls {
+    display: flex;
+}
+.input {
+    display: block;
+    transition: all 1s;
+}
+.list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateX(3rem);
+}
+.list-leave-active {
+    position: absolute;
+}
+
 </style>
