b8cf6c59312ef3d72f7d31c241d2d5598cc0f209
vanderkilu <vanderkiliku@gmail.com>
2019-06-17 14:08:17 -0400

    refactor code to one liner

--- src/ui/src/api/index.js
+++ src/ui/src/api/index.js
@@ -11,47 +11,18 @@ const axios = Axios.create({
     baseURL: 'http://localhost:3000',
 })
 
-const login = (data) => {
-    return axios.post('/api/account/login', data)
-}
-
-const signup = (data)=> {
-    return axios.post('/api/account/register', data)
-}
-
-const getAllCategory = () => {
-    return axios.get('/api/poll/categories', headers())
-}
-
-const getAllPoll = () => {
-    return axios.get('/api/poll/', headers())
-}
-const getAllPollsForCategory = (id) => {
-    return axios.get(`/api/poll/category/${id}/polls`, headers())
-}
-const getPoll = (id) => {
-    return axios.get(`/api/poll/${id}`, headers())
-}
-
-const getAllOptionsForPoll = (id) => {
-    return axios.get(`/api/poll/${id}/polloptions`, headers())
-}
-
-const doVote = (pollId, pollOptionId) => {
-    return axios.post(`/api/poll/${pollId}/vote`, {id: pollOptionId}, headers())
-}
-
-const createPoll = (data) => {
-    return axios.post('/api/poll/create', data, headers())
-}
-const getRecentPolls = () => { console.log(headers()) 
-    return axios.get('/api/poll/recent', headers()) }
-
+const login = (data) => axios.post('/api/account/login', data)
+const signup = (data)=> axios.post('/api/account/register', data)
+const getAllCategory = () => axios.get('/api/poll/categories', headers())
+const getAllPoll = () => axios.get('/api/poll/', headers())
+const getAllPollsForCategory = (id) => axios.get(`/api/poll/category/${id}/polls`, headers())
+const getPoll = (id) => axios.get(`/api/poll/${id}`, headers())
+const getAllOptionsForPoll = (id) => axios.get(`/api/poll/${id}/polloptions`, headers())
+const doVote = (pollId, pollOptionId) => axios.post(`/api/poll/${pollId}/vote`, {id: pollOptionId}, headers())
+const createPoll = (data) => axios.post('/api/poll/create', data, headers())
+const getRecentPolls = () =>  axios.get('/api/poll/recent', headers())
 const updateCount = (pollId) => axios.put(`/api/poll/${pollId}/update-count`, {}, headers())
-
-const getAllComment = (pollId)=> {
-    return axios.get(`/api/poll/${pollId}/comments`, headers())
-}
+const getAllComment = (pollId)=> axios.get(`/api/poll/${pollId}/comments`, headers())
 const commentCreate = (pollId, data) => axios.post(`/api/poll/${pollId}/comments`,data, headers())
 const likeToggle = (commentId) => axios.put(`/api/poll/comment/likes`, {id: commentId}, headers())
 const getPopularPolls = () => axios.get(`/api/poll/popular`, headers())
