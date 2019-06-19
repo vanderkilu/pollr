3d4c61085740c0970826e1449ab2750025903718
vanderkilu <vanderkiliku@gmail.com>
2019-06-18 21:09:32 -0400

    adding loading animation to category

--- src/ui/src/components/Polls.vue
+++ src/ui/src/components/Polls.vue
@@ -1,27 +1,34 @@
 <template>
-   <app-poll-card :polls="polls" path="poll"></app-poll-card>
+    <div>
+        <app-poll-card :polls="polls" path="poll"></app-poll-card>
+        <app-loader v-show="isLoading"></app-loader>
+   </div>
 </template>
 
 <script>
 import { getAllPollsForCategory } from '../api'
 import PollCard from './PollCard.vue'
+import Loader from './Loader'
 export default {
     data() {
         return {
-            polls: []
+            polls: [],
+            isLoading: true
         }
     },
     methods: {
         async getPolls() {
             const data = await getAllPollsForCategory(this.$route.params.id)
             this.polls = data.data
+            this.isLoading = false
         },
     },
     mounted() {
         this.getPolls()
     },
     components: {
-        appPollCard: PollCard
+        appPollCard: PollCard,
+        appLoader: Loader
     }
 }
     
